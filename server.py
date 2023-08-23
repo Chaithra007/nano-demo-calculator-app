5c52afca25fad3ba8a40a351a1ac02d55a7e2424
Chaithra <chaithrausha2002@gmail.com>
2023-08-23 23:37:07 +0530

    commit changes

--- server.py
+++ server.py
@@ -1,19 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.json  # Get JSON data from the request
+    if 'a' in data and 'b' in data:
+        a = data['a']
+        b = data['b']
+        result = a + b
+        return jsonify({'result': result})
+    else:
+        return jsonify({'error': 'Missing parameters'}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.json  # Get JSON data from the request
+    if 'a' in data and 'b' in data:
+        a = data['a']
+        b = data['b']
+        result = a - b
+        return jsonify({'result': result})
+    else:
+        return jsonify({'error': 'Missing parameters'}), 400
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
